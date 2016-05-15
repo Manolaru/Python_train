acf8e58e2581fe3c2889b551a8fc6d84d2633c8d
Manolaru <manolaru@yahoo.co.uk>
2016-05-15 22:28:33 +0300

    Working version -> WorkingVersion

--- Working version/test/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-
-from model.group import Group
-
-def test_edit_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="testing"))
-    app.group.edit_first_group()
-
--- Working version/test/test_edit_user.py
+++
@@ -1,8 +0,0 @@
-from model.user import User
-
-
-def test_edit_user(app):
-    if app.user.count() == 0:
-        app.user.create(User(lname="Testman"))
-    app.user.edit_first_user()
-
